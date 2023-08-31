d824bafee44bbd4053246b1c5d2d2bd1e16642b1
sweety <122354455+raghvikumari@users.noreply.github.com>
2023-08-31 14:02:40 +0530

    initial commit

---
+++ client/src/components/BlogCard.js
@@ -0,0 +1,170 @@
+// import * as React from 'react';
+// import { styled } from '@mui/material/styles';
+// import Card from '@mui/material/Card';
+// import CardHeader from '@mui/material/CardHeader';
+// import CardMedia from '@mui/material/CardMedia';
+// import CardContent from '@mui/material/CardContent';
+// import CardActions from '@mui/material/CardActions';
+// import Collapse from '@mui/material/Collapse';
+// import Avatar from '@mui/material/Avatar';
+// import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+// import Typography from '@mui/material/Typography';
+// import { red } from '@mui/material/colors';
+
+// import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+// interface ExpandMoreProps extends IconButtonProps {
+//   expand: boolean;
+// }
+
+
+
+
+ 
+
+//   return (
+//     <Card sx={{ maxWidth: 345 }}>
+//       <CardHeader
+//         avatar={
+//           <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+//             R
+//           </Avatar>
+//         }
+//         action={
+//           <IconButton aria-label="settings">
+//             <MoreVertIcon />
+//           </IconButton>
+//         }
+//         title="Shrimp and Chorizo Paella"
+//         subheader="September 14, 2016"
+//       />
+//       <CardMedia
+//         component="img"
+//         height="194"
+//         image="/static/images/cards/paella.jpg"
+//         alt="Paella dish"
+//       />
+//       <CardContent>
+//         <Typography variant="body2" color="text.secondary">
+//           This impressive paella is a perfect party dish and a fun meal to cook
+//           together with your guests. Add 1 cup of frozen peas along with the mussels,
+//           if you like.
+//         </Typography>
+//       </CardContent>
+     
+//       <Collapse in={expanded} timeout="auto" unmountOnExit>
+//         <CardContent>
+//           <Typography paragraph>Method:</Typography>
+//           <Typography paragraph>
+//             Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
+//             aside for 10 minutes.
+//           </Typography>
+//           <Typography paragraph>
+//             Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
+//             medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
+//             occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
+//             large plate and set aside, leaving chicken and chorizo in the pan. Add
+//             pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
+//             stirring often until thickened and fragrant, about 10 minutes. Add
+//             saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+//           </Typography>
+//           <Typography paragraph>
+//             Add rice and stir very gently to distribute. Top with artichokes and
+//             peppers, and cook without stirring, until most of the liquid is absorbed,
+//             15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
+//             mussels, tucking them down into the rice, and cook again without
+//             stirring, until mussels have opened and rice is just tender, 5 to 7
+//             minutes more. (Discard any mussels that don&apos;t open.)
+//           </Typography>
+//           <Typography>
+//             Set aside off of the heat to let rest for 10 minutes, and then serve.
+//           </Typography>
+//         </CardContent>
+//       </Collapse>
+//     </Card>
+//   );
+// }
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Box, IconButton } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
+
+export default function BlogCard({
+  title,
+  description,
+  image,
+  username,
+  time,
+  id,
+  isUser,
+}) {
+  const navigate = useNavigate();
+  const handleEdit = () => {
+    navigate(`/blog-details/${id}`);
+  };
+
+  const handleDelete = async () => {
+    try {
+      const { data } = await axios.delete(`/api/v1/blog/delete-blog/${id}`);
+      if (data?.success) {
+        alert("Blog Deleted");
+        window.location.reload();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <Card
+      sx={{
+        width: "40%",
+        margin: "auto",
+        mt: 2,
+        padding: 2,
+        boxShadow: "5px 5px 10px #ccc",
+        ":hover:": {
+          boxShadow: "10px 10px 20px #ccc",
+        },
+      }}
+    >
+      {isUser && (
+        <Box display={"flex"}>
+          <IconButton onClick={handleEdit} sx={{ marginLeft: "auto" }}>
+            <ModeEditIcon color="info" />
+          </IconButton>
+          <IconButton onClick={handleDelete}>
+            <DeleteIcon color="error" />
+          </IconButton>
+        </Box>
+      )}
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            {username}
+          </Avatar>
+        }
+        title={username}
+        subheader={time}
+      />
+      <CardMedia component="img" height="194" image={image} alt="Paella dish" />
+      <CardContent>
+        <Typography variant="h6" color="text.secondary">
+          Title : {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Description : {description}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
